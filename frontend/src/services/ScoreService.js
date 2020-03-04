cacf65be87ea793c29c12f21072b2024ef8f5f2c
Mikael <mikael.heininen@student.uwasa.fi>
2020-03-04 15:28:24 +0200

    backslash removed from url

--- frontend/src/services/ScoreService.js
+++ frontend/src/services/ScoreService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://shrouded-chamber-27182.herokuapp.com/"
+const url = "https://shrouded-chamber-27182.herokuapp.com"
 
 const getScores = () => {
     return axios.get(url + "/scores")
